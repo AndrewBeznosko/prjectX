e1d4255939c5a81217376c1ad5d26ef6a21ed65b
Anton <anton.vakulenchyk@gmail.com>
2018-07-03 11:42:26 +0300

    sec-left svg icons, .checked and change color on click

--- js/main.js
+++ js/main.js
@@ -6,5 +6,16 @@ $(document).ready(function () {
             $(this).addClass("selected").siblings().removeClass("selected");
         });
     });
+    /* sec-left bookmark check */
+    $(".bookmark").each(function() {
+       $(this).click(function() {
+           if($(this).parents("li.item").hasClass("checked")) {
+               $(this).parents("li.item").removeClass("checked");
+           } else 
+          $(this).parents("li.item").addClass("checked");
+           
+       });
+        
+    });
 
 });
