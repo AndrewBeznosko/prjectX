43890ea5785b657adfbf9d9c9abb0d3f1c5bef50
andrew.beznosko <andrew.beznosko@gmail.com>
2018-07-06 01:17:24 +0300

    sec-center-3 table header fixed

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
-    
+
     // Select money
     $("#js-select-money .item").each(function () {
         $(this).click(function () {
             $(this).addClass("selected").siblings().removeClass("selected");
         });
     });
-    
+
     $("#js-chart_tab .item").each(function () {
         $(this).click(function () {
             $(this).addClass("selected").siblings().removeClass("selected");
@@ -19,34 +19,36 @@ $(document).ready(function () {
         });
     });
     /* sec-left bookmark check */
-    $(".bookmark").each(function() {
-       $(this).click(function() {
-           if($(this).parents("li.item").hasClass("checked")) {
-               $(this).parents("li.item").removeClass("checked");
-           } else 
-          $(this).parents("li.item").addClass("checked");
-           
-       });
-        
+    $(".bookmark").each(function () {
+        $(this).click(function () {
+            if ($(this).parents("li.item").hasClass("checked")) {
+                $(this).parents("li.item").removeClass("checked");
+            } else
+                $(this).parents("li.item").addClass("checked");
+
+        });
+
     });
-    
+
     /* sec-center bell check */
-    $(".bell_wrapp").each(function() {
-       $(this).click(function() {
-           if($(this).parents("tr").hasClass("checked")) {
-               $(this).parents("tr").removeClass("checked");
-           } else 
-          $(this).parents("tr").addClass("checked");
-           
-       });
-        
+    $(".bell_wrapp").each(function () {
+        $(this).click(function () {
+            if ($(this).parents("tr").hasClass("checked")) {
+                $(this).parents("tr").removeClass("checked");
+            } else
+                $(this).parents("tr").addClass("checked");
+
+        });
+
     });
 });
 
 // calculate table height
-$(window).on("load resize ", function() {
-    var jsWrapperReadyHeight = $(".js-wrapper-ready-height").offset().top + $(".js-wrapper-ready-height").outerHeight();
-    var jsTableHeight = $(".js-table-height").offset().top;
-    var jsTableHeight_new = jsWrapperReadyHeight - jsTableHeight;
-    $(".js-table-height").css("height", jsTableHeight_new);
+$(window).on("load resize ", function () {
+    $(".js-wrapper-ready-height").each(function () {
+        var jsWrapperReadyHeight = $(this).offset().top + $(this).outerHeight();
+        var jsTableHeight = $(this).find(".js-table-height").offset().top;
+        var jsTableHeight_new = jsWrapperReadyHeight - jsTableHeight;
+        $(this).find(".js-table-height").css("height", jsTableHeight_new);
+    });
 }).resize();
